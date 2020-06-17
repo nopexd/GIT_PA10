81650f754bc3d4eee8a5addbdfb4846436b9d161
nopexd <64115016+nopexd@users.noreply.github.com>
2020-06-17 21:01:01 +0800

    yes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
-
+    //public float speed = 2;
+    public float force = 100;
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+      // Rigidbody= GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector2.up * force);
             thisAnimation.Play();
+        }
+
+      
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        
+
+
+        if (other.tag == "obstacle")
+        {
+            SceneManager.LoadScene("restart");
+
+        }
+
     }
 }
